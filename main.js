eb47a554d017aa9979789995b763ee03808c03c1
kingkong1106 <78448474+kingkong1106@users.noreply.github.com>
2022-04-22 16:44:45 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,46 +1,19 @@
-noseX = 0;
-noseY = 0;
-difference = 0;
-rightWristX = 0;
-leftWristX = 0;
+song = "";
+song2 = "";
 function preload()
 {
-
+song = loadSound("music.mp3");
+song2 = loadSound("music2.mp3")
 }
 function setup()
 {
+canvas = createCanvas(600,500);
+canvas.center();
 video = createCapture(VIDEO);
-video.size(550,500);
-canvas = createCanvas(550,550);
-canvas.position(560,150);
-poseNet = ml5.poseNet(video, modelLoaded);
-poseNet.on('pose', gotPoses);
-
-}
-function modelLoaded()
-{
-    console.log("poseNet is loaded!");
-}
-function gotPoses(results){
-    if(results.length > 0)
-    {
-        console.log(results);
-        noseX = results[0].pose.nose.x;
-    noseY = results[0].pose.nose.y;
-    console.log("noseX = " + noseX + "noseY " + noseY);
-    leftWristX = results[0].pose.leftWrist.x;
-    rightWristX = results[0].pose.rightWrist.x;
-    difference = floor(leftWristX - rightWristX);
-    console.log("leftWristX = " + leftWristX + "rightWristX" + rightWristX + "difference = " + difference);
-    }
-
+video.hide();
 }
 function draw()
 {
-  background('#02AD97');
-  textSize(32);
-  text('Barath', 10, 30);
-  fill(90, 45, 192);
-  text(noseX, noseY, difference);
-  
+image(video, 0, 0, 600, 500);
 }
+ 
